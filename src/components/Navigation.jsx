46f35ce48a567e57a60e51b26c4a2021e2c85f12
Beaubien, Jase A <jasebeaubien@gmail.com>
2025-04-25 16:21:14 -0500

    commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,12 +3,12 @@ import {useState} from 'react';
 
 const Navigation = ({setStep}) => {
     //let buttonData = "btn mt-8 !font-serif text-amber-100 rounded-none !bg-transparent hover:!inset-shadow-sm hover:!inset-shadow-amber-100 transition-shadow duration-300";
-    let buttonData = "btn !text-xl xl:!text-2xl !w-1/2 m-1 !p-2 xl:!p-4 !border-none focus:!border-none hover:!border-none !font-serif !rounded-md !bg-transparent !shadow-[inset_0px_-5px_2px_rgba(0,0,0,0.3)] hover:!shadow-[inset_0px_-3px_2px_rgba(180,170,127,1)] active:!shadow-[inset_0px_-3px_5px_rgba(180,170,127,1)] active:!bg-black/15 transition-shadow duration-300";
+    let buttonData = "btn !text-3xl xl:!text-2xl !w-1/2 m-1 !p-2 xl:!p-4 !border-none focus:!border-none hover:!border-none !font-serif !rounded-md !bg-transparent !shadow-[inset_0px_-5px_2px_rgba(0,0,0,0.3)] hover:!shadow-[inset_0px_-3px_2px_rgba(180,170,127,1)] active:!shadow-[inset_0px_-3px_5px_rgba(180,170,127,1)] active:!bg-black/15 transition-shadow duration-300";
     return(
         <>
             <header className="z-200 fixed top-0 left-0 shadow-md w-full bg-primary justify-center items-center">
                 <div className="flex flex-col xl:flex-row items-center justify-between">
-                    <p className="w-full flex xl:w-1/2 font-serif text-2xl xl:text-4xl mt-2 mb-0 xl:m-4 xl:ml-8 justify-center xl:justify-start">Jase Beaubien</p>
+                    <p className="w-full flex xl:w-1/2 font-serif text-4xl xl:text-4xl mt-2 mb-0 xl:m-4 xl:ml-8 justify-center xl:justify-start">Jase Beaubien</p>
                     <div className="flex m-1 mt-0 flex xl:justify-end xl:items-end justify-center items-center">
                         <button onClick={() => setStep("about")} className={buttonData}>About</button>
                         <button onClick={() => setStep("projects")} className={buttonData}>Projects</button>
